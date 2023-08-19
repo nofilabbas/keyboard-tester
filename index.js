0cdbdfd927bce59c4c297066cbbd6ae4e84af57f
Nofil Abbas <111782156+nofilabbas@users.noreply.github.com>
2023-08-19 12:59:19 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,261 @@
+document.addEventListener("keydown", function (event) {
+  console.log(event.keyCode);
+  switch (event.keyCode) {
+    case 8:
+      document.getElementById(8).classList.add("pressed");
+      break;
+    case 9:
+      document.getElementById(9).classList.add("pressed");
+      break;
+    case 13:
+      document.getElementById(13).classList.add("pressed");
+      break;
+    case 16:
+      if (event.code === "ShiftLeft") {
+        document.getElementById("16-left").classList.add("pressed");
+        break;
+      } else {
+        document.getElementById("16-right").classList.add("pressed");
+        break;
+      }
+    case 17:
+      if (event.code === "ControlLeft") {
+        document.getElementById("17-left").classList.add("pressed");
+        break;
+      } else {
+        document.getElementById("17-right").classList.add("pressed");
+        break;
+      }
+    case 18:
+      if (event.code === "AltLeft") {
+        document.getElementById("18-left").classList.add("pressed");
+        break;
+      } else {
+        document.getElementById("18-right").classList.add("pressed");
+        break;
+      }
+    case 20:
+      document.getElementById(20).classList.add("pressed");
+      break;
+    case 27:
+      document.getElementById(27).classList.add("pressed");
+      break;
+    case 32:
+      document.getElementById(32).classList.add("pressed");
+      break;
+    case 33:
+      document.getElementById(33).classList.add("pressed");
+      break;
+    case 34:
+      document.getElementById(34).classList.add("pressed");
+      break;
+    case 35:
+      document.getElementById(35).classList.add("pressed");
+      break;
+    case 36:
+      document.getElementById(36).classList.add("pressed");
+      break;
+    case 37:
+      document.getElementById(37).classList.add("pressed");
+      break;
+    case 38:
+      document.getElementById(38).classList.add("pressed");
+      break;
+    case 39:
+      document.getElementById(39).classList.add("pressed");
+      break;
+    case 40:
+      document.getElementById(40).classList.add("pressed");
+      break;
+    case 45:
+      document.getElementById(45).classList.add("pressed");
+      break;
+    case 46:
+      document.getElementById(46).classList.add("pressed");
+      break;
+    case 48:
+      document.getElementById(48).classList.add("pressed");
+      break;
+    case 49:
+      document.getElementById(49).classList.add("pressed");
+      break;
+    case 50:
+      document.getElementById(50).classList.add("pressed");
+      break;
+    case 51:
+      document.getElementById(51).classList.add("pressed");
+      break;
+    case 52:
+      document.getElementById(52).classList.add("pressed");
+      break;
+    case 53:
+      document.getElementById(53).classList.add("pressed");
+      break;
+    case 54:
+      document.getElementById(54).classList.add("pressed");
+      break;
+    case 55:
+      document.getElementById(55).classList.add("pressed");
+      break;
+    case 56:
+      document.getElementById(56).classList.add("pressed");
+      break;
+    case 57:
+      document.getElementById(57).classList.add("pressed");
+      break;
+    case 65:
+      document.getElementById(65).classList.add("pressed");
+      break;
+    case 66:
+      document.getElementById(66).classList.add("pressed");
+      break;
+    case 67:
+      document.getElementById(67).classList.add("pressed");
+      break;
+    case 68:
+      document.getElementById(68).classList.add("pressed");
+      break;
+    case 69:
+      document.getElementById(69).classList.add("pressed");
+      break;
+    case 70:
+      document.getElementById(70).classList.add("pressed");
+      break;
+    case 71:
+      document.getElementById(71).classList.add("pressed");
+      break;
+    case 72:
+      document.getElementById(72).classList.add("pressed");
+      break;
+    case 73:
+      document.getElementById(73).classList.add("pressed");
+      break;
+    case 74:
+      document.getElementById(74).classList.add("pressed");
+      break;
+    case 75:
+      document.getElementById(75).classList.add("pressed");
+      break;
+    case 76:
+      document.getElementById(76).classList.add("pressed");
+      break;
+    case 77:
+      document.getElementById(77).classList.add("pressed");
+      break;
+    case 78:
+      document.getElementById(78).classList.add("pressed");
+      break;
+    case 79:
+      document.getElementById(79).classList.add("pressed");
+      break;
+    case 80:
+      document.getElementById(80).classList.add("pressed");
+      break;
+    case 81:
+      document.getElementById(81).classList.add("pressed");
+      break;
+    case 82:
+      document.getElementById(82).classList.add("pressed");
+      break;
+    case 83:
+      document.getElementById(83).classList.add("pressed");
+      break;
+    case 84:
+      document.getElementById(84).classList.add("pressed");
+      break;
+    case 85:
+      document.getElementById(85).classList.add("pressed");
+      break;
+    case 86:
+      document.getElementById(86).classList.add("pressed");
+      break;
+    case 87:
+      document.getElementById(87).classList.add("pressed");
+      break;
+    case 88:
+      document.getElementById(88).classList.add("pressed");
+      break;
+    case 89:
+      document.getElementById(89).classList.add("pressed");
+      break;
+    case 90:
+      document.getElementById(90).classList.add("pressed");
+      break;
+    case 91:
+      document.getElementById(91).classList.add("pressed");
+      break;
+    case 93:
+      document.getElementById(93).classList.add("pressed");
+      break;
+
+    case 112:
+      document.getElementById(112).classList.add("pressed");
+      break;
+    case 113:
+      document.getElementById(113).classList.add("pressed");
+      break;
+    case 114:
+      document.getElementById(114).classList.add("pressed");
+      break;
+    case 115:
+      document.getElementById(115).classList.add("pressed");
+      break;
+    case 116:
+      document.getElementById(116).classList.add("pressed");
+      break;
+    case 117:
+      document.getElementById(117).classList.add("pressed");
+      break;
+    case 118:
+      document.getElementById(118).classList.add("pressed");
+      break;
+    case 119:
+      document.getElementById(119).classList.add("pressed");
+      break;
+    case 120:
+      document.getElementById(120).classList.add("pressed");
+      break;
+    case 121:
+      document.getElementById(121).classList.add("pressed");
+      break;
+    case 122:
+      document.getElementById(122).classList.add("pressed");
+      break;
+    case 123:
+      document.getElementById(123).classList.add("pressed");
+      break;
+    case 186:
+      document.getElementById(186).classList.add("pressed");
+      break;
+    case 187:
+      document.getElementById(187).classList.add("pressed");
+      break;
+    case 188:
+      document.getElementById(188).classList.add("pressed");
+      break;
+    case 189:
+      document.getElementById(189).classList.add("pressed");
+      break;
+    case 190:
+      document.getElementById(190).classList.add("pressed");
+      break;
+    case 191:
+      document.getElementById(191).classList.add("pressed");
+      break;
+    case 192:
+      document.getElementById(192).classList.add("pressed");
+      break;
+    case 219:
+      document.getElementById(219).classList.add("pressed");
+      break;
+    case 220:
+      document.getElementById(220).classList.add("pressed");
+      break;
+    case 221:
+      document.getElementById(221).classList.add("pressed");
+      break;
+    case 222:
+      document.getElementById(222).classList.add("pressed");
+      break;
+  }
+});
